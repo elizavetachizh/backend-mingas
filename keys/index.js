6d5017236a491be43c803605adb849a92dc8ae0a
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2023-07-11 09:56:14 +0300

    feat: add functional to project

--- keys/index.js
+++ keys/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   MONGODB_URI:
-    "mongodb://root:v|}BfYvNXhQed5yH@mongo:27017",
+    "mongodb+srv://elizavetachizh:mwSF7rcHYSd0XO2Z@cluster0.ct1ltqh.mongodb.net/?retryWrites=true&w=majority",
   SESSION_SECRET: "some secret value",
 };
 // MONGODB_URI:
